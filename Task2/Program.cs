e3e02009673ecb29db9992a4ba7c3ea962d91178
Andr3wBro <andr3wbro@gmail.com>
2022-06-21 20:55:12 +0300

    home work done

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] CreateAndFillArr(int large, int min, int max)
+{
+    int[] array = new int[large];
+    for (int ifill = 0; ifill < array.Length; ifill++)
+    {
+        array[ifill] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+void printArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}  ");
+    }
+    System.Console.WriteLine();
+}
+
+int SumOddPositions (int[] arr)
+{
+    int SumOdd = 0;
+    for(int i=0; i<arr.Length;i+=2)
+    {
+        SumOdd += arr[i];    
+    }
+System.Console.WriteLine($"Sum Odd Positions in array => {SumOdd}");
+return SumOdd;
+}
+
+
+int[] myArray = CreateAndFillArr(10,0,10);
+printArr(myArray);
+SumOddPositions(myArray);
---
+++ Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+int[] CreateAndFillArr(int large, int min, int max)
+{
+    int[] array = new int[large];
+    for (int ifill = 0; ifill < array.Length; ifill++)
+    {
+        array[ifill] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+void printArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}  ");
+    }
+    System.Console.WriteLine();
+}
+
+int FindDiferenceBetweenMinMax(int[] arr)
+{
+    int min = arr[0];
+    int max = arr[0];
+    int diference = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        else
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    diference = max - min;
+    System.Console.WriteLine($" Max is {max} ,  Min is {min} , diference betwen max and min =  , {diference}");
+    return diference;
+}
+
+
+int[] myArray = CreateAndFillArr(10, 0, 50);
+printArr(myArray);
+FindDiferenceBetweenMinMax(myArray);
